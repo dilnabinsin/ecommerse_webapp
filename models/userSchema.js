9efd4487ceac671bafc8502368ef9bcbc9ee1acc
dilnabinsin <57486554+dilnabinsin@users.noreply.github.com>
2025-05-15 10:56:29 +0530

    Update userSchema.js
    email added

--- models/userSchema.js
+++ models/userSchema.js
@@ -9,6 +9,11 @@ const userSchema = new Schema({
        type:String,
        required : true
    },
+   email:{
+    type: String,
+    required: true,
+    unique: true
+},
    username: {
     type:String,
     required:false
