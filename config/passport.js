816c896687c42f985e46ef7f2fae38edd3f5e755
dilnabinsin <57486554+dilnabinsin@users.noreply.github.com>
2025-05-15 11:00:27 +0530

    Update passport.js
    googleauthentication url changed

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env = require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/auth/google/callback'
+    callbackURL: 'https://tksarestudio.shop/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         console.log("✅ Google OAuth Profile:", profile);
